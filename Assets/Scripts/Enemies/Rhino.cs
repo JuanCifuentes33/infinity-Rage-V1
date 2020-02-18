e7ae79d205e4bbbc2d48e7ee5a0005b3530e4a76
Cifuenteesss <juan.cifuentes@hotmail.com>
2020-02-18 23:10:58 +0100

    rhino beta and water
    rh

---
+++ Assets/Scripts/Enemies/Rhino.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rhino : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+    private Transform player;
+    [Range(3,20)]public float range = 1f;
+    [Range(1, 3)] public float heightRange = 2f;
+    [Range(1, 4)] public float speed = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+         player= GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CompruebaFlipeo();
+    }
+    private void FixedUpdate()
+    {
+        float target_dist = (transform.position - player.position).sqrMagnitude;
+        if (Vector2.Distance(transform.position, player.position)<range && transform.position.y <= player.transform.position.y + heightRange)
+        {
+            Debug.Log("dentroo");
+            rb2D.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+
+        }
+
+    }
+
+    private void CompruebaFlipeo()
+    {
+        if ( transform.position.x - player.position.x  < 0)
+        {
+            rb2D.transform.rotation = new Quaternion(0, 180, 0, 0);
+        }
+        else
+        {
+            rb2D.transform.rotation = new Quaternion(0, 0, 0, 0); ;
+        }
+    }
+
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        if (transform.localRotation.y > 0)
+        {
+            Gizmos.DrawLine(transform.position, new Vector2(transform.position.x + range, transform.position.y));
+
+        }
+        else
+        {
+            Gizmos.DrawLine(transform.position, new Vector2(transform.position.x - range, transform.position.y));
+
+        }
+
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x, transform.position.y + heightRange));
+    }
+}
---
+++ Assets/Scripts/movimientoCamara.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimientoCamara : MonoBehaviour
+{
+    //Define la zona muerta(por defecto un 50%)
+    [Range(0f, 1f)] public float deadZoneFactor = 0.5f;
+    [Range(0f, 1f)] public float smoothFactor = 0.1f;
+
+    //es la mitad de la altura y anchura de la dead zone
+    private float deadZoneHeight, deadZoneWidth;
+
+    private Transform target;
+    private Bounds limiteZonaMuerta;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        deadZoneHeight = Camera.main.orthographicSize * deadZoneFactor;
+        deadZoneWidth = deadZoneHeight * Camera.main.aspect;
+        limiteZonaMuerta = new Bounds(transform.position, new Vector3(deadZoneWidth * 2, deadZoneHeight * 2, 1f));
+
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        limiteZonaMuerta = new Bounds(transform.position, new Vector3(deadZoneWidth * 2, deadZoneHeight * 2, 1f));
+        float incrX, incrY;
+        if (!IsIndeadZone(out incrX, out incrY))
+        {
+            transform.position = Vector3.Lerp(transform.position, transform.position + new Vector3(incrX, incrY, 0f), smoothFactor);
+        }
+
+
+
+    }
+
+    private bool IsIndeadZone(out float incrX, out float incrY)
+    {
+        limiteZonaMuerta.center = transform.position;
+        if (!limiteZonaMuerta.Contains(target.position))
+        {
+            //guardanos el punto mas cercano del target al cubo
+            Vector3 closestPoint = limiteZonaMuerta.ClosestPoint(target.position);
+            incrX = target.position.x - closestPoint.x;
+            incrY = target.position.y - closestPoint.y;
+
+            return false;
+        }
+        incrX = 0;
+        incrY = 0;
+        return true;
+
+    }
+    private void OnDrawGizmosSelected()
+    {
+        deadZoneHeight = Camera.main.orthographicSize * deadZoneFactor;
+        deadZoneWidth = deadZoneHeight * Camera.main.aspect;
+
+        //Dibujara una linea que significara la zona muerta.La primera es para darle un color y la segunda es para dibujar una linea
+        Gizmos.color = Color.magenta;
+
+        // Gizmos.DrawLine(transform.position, transform.position + new Vector3(0f, deadZoneHeight, 0f));
+
+        Gizmos.DrawWireCube(transform.position, new Vector3(2 * deadZoneWidth, deadZoneHeight * 2.0f));
+
+    }
+}
